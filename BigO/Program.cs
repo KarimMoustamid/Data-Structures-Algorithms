e3cc4c3941d7675b63f3144bacf18a0ae57198e2
karimmoustamid <moustamid.teck@gmail.com>
2024-01-04 09:52:19 -0600

    Add Nemo characters array and update method call
    An array called 'nemoCharacters' has been added to the code. The array entries correspond to characters from the "Finding Nemo" franchise. Consequently, the method call of 'findNemo' has been adjusted to utilize 'nemoCharacters' instead of the previous array.

--- BigO/Program.cs
+++ BigO/Program.cs
@@ -1,20 +1,46 @@
 ﻿using System;
 using System.Diagnostics;
 
-// ! In C#, you can use classes from the System.Diagnostics namespace to create a stopwatch timer. It can be used to measure elapsed time with a high precision.
+
+string[] nemoCharacters = new string[]
+{
+    "Nemo",
+    "Marlin",
+    "Dory",
+    "Gill",
+    "Bloat",
+    "Peach",
+    "Gurgle",
+    "Bubbles",
+    "Deb",
+    "Jacques",
+    "Nigel",
+    "Crush",
+    "Squirt",
+    "Bruce",
+    "Anchor",
+    "Chum"
+};
+
+#region Performance Using Time
+
+// In C#, you can use classes from the System.Diagnostics namespace to create a stopwatch timer. It can be used to measure elapsed time with a high precision.
 
 Stopwatch stopwatch = new Stopwatch();
-stopwatch.Start(); 
+stopwatch.Start();
+
 // Put logic that you want to measure here, 
-string[] Array = {"nemo", "dani"};
-findNemo(Array);
+findNemo(nemoCharacters);
 stopwatch.Stop();
+
 Console.WriteLine("Elapsed time: {0} seconds", stopwatch.Elapsed.TotalSeconds);
+
+#endregion
 
 Console.ReadKey();
 
 
-
+#region Methods
 
 void findNemo(string[] Array)
 {
@@ -25,18 +51,11 @@ void findNemo(string[] Array)
             Console.WriteLine("Found Nemo");
         }
     }
-
 }
 
+#endregion
 
-
-
-
-
-
-
-
-
+#region Explanation 
 
 /*
    Big O notation is a mathematical notation that describes the limiting behavior of a function
@@ -64,3 +83,5 @@ void findNemo(string[] Array)
    It's important to note that Big O notation represents the worst-case scenario of an algorithm's time complexity.
    While it's a useful tool for analyzing and comparing algorithms, it doesn't provide a complete picture of an algorithm's performance in real-world use cases.
  */
+
+#endregion
